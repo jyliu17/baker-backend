deef4425b31155c1493fd00e99d48f53cfee7ccf
jyliu17 <jyliu17@gmail.com>
2021-02-12 17:00:20 -0500

    updated serializers

--- app/serializers/baker_serializer.rb
+++ app/serializers/baker_serializer.rb
@@ -1,3 +1,5 @@
 class BakerSerializer < ActiveModel::Serializer
-    attributes :id, :name, :location, :contact, :profile_image, :expertise, :pastries
+    attributes :id, :name, :location, :contact, :profile_image, :expertise 
+    
+    has_many :pastries
 end
