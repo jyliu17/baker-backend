64e2fdf8e4a53ded38adc3b538fb46b4a1e4c365
jyliu17 <jyliu17@gmail.com>
2021-02-17 20:56:03 -0500

    updated some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,9 @@ User.create(username: "johnson", password: "cookies123")
 User.create(username: "noura", password: "cakes")
 
 # ************BAKERS***************
+
+
+Baker.create(name: "Lisa's Cakes", location: "41-29 162st Flushing, NY 11358", contact: "(929) 281-3278", profile_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Paul_Hollywood.jpg/440px-Paul_Hollywood.jpg", expertise: "Custom Cakes")
 Baker.create(name: "Paul Hollywood", location: "NYC, Hudson Heights", contact: "john@gmail.com", profile_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Paul_Hollywood.jpg/440px-Paul_Hollywood.jpg", expertise: "Cake Boss")
 Baker.create(name: "Candice Brown", location: "SF, Mission District", contact: "clark@gmail.com", profile_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRthOzB1DdcWQlqxX2I9-i47wUV6NSenDq_0g&usqp=CAU", expertise: "French")
 Baker.create(name: "Nadiya Hussain", location: "LA, Santa Monica", contact: "jen@gmail.com", profile_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNfzfMiv6pA0wUpa5xYzVuRjMAKX75lMYauQ&usqp=CAU", expertise: "Chocolate Bars")
@@ -18,16 +21,21 @@ Baker.create(name: "Nadiya Hussain", location: "LA, Santa Monica", contact: "jen
 # ************FAVORITES***************
 # Favorite.create(user_id: 1, baker_id: 1)
 # Favorite.create(user_id: 1, baker_id: 2)
-# Favorite.create(user_id: 2, baker_id: 2)
+# Favorite.creat:e(user_id: 2, baker_id: 2)
 # Favorite.create(user_id: 2, baker_id: 3)
 
 # ************PASTRIES***************
-Pastry.create(baker_id: 1, name: "Birthday Cake", description: "Disney Theme Cake", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPbQAJGh-oeW_inDwYn51vpLaCgR0VK5YWng&usqp=CAU")
-Pastry.create(baker_id: 2, name: "Croissant", description: "A buttery, flaky, viennoiserie French pastry", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcMuC5bzEL_ecE-5AHZ7aSbuS5GCy_W5OHcQ&usqp=CAU")
-Pastry.create(baker_id: 2, name: "Napoleon", description: "made of flaky, buttery puff pastry layered between sweet vanilla custard.", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeiFSlmmgXOP1UFgy51SNSED6JerGbYmxUVA&usqp=CAU")
-Pastry.create(baker_id: 1, name: "Rainbow Cake", description: "Not only is this rainbow cake beautiful and colorful, but it also tastes really delicious. Made from my famous white velvet cake recipe and easy buttercream, this rainbow cake makes the perfect special occasion cake!", image: "https://sugargeekshow.com/wp-content/uploads/2020/03/rainbow-cake-featured-scaled.jpg")
+Pastry.create(baker_id: 1, name: "Disney Cake", description: "Disney Theme Cake", image: "https://lh3.googleusercontent.com/pw/ACtC-3cKQvvVMtYnIbkpdx3vX6sflSJJWhuPZygMKwX2BuaGDE3NZkq2JO7CLKvtjNZV-IajyIy2UDMrtWhkkc_yM0T91HA1RoN48iBIcyF5JxgFs3LUyBsn44_iItD1_ukiy7RMHF9ayqiDd0N9x_rOb2eo=w406-h540-no?authuser=0")
+Pastry.create(baker_id: 1, name: "Louis Vuitton Cake", description: "Luxury upon luxury", image: "https://s3-media0.fl.yelpcdn.com/bphoto/31jFgGbuqLzNl-YWPPK-CQ/o.jpg")
+Pastry.create(baker_id: 1, name: "Super Mario", description: "It's-A Me, Mario!", image: "https://s3-media0.fl.yelpcdn.com/bphoto/wIbGjF4xRg0tJLRqQkPb6g/o.jpg")
+Pastry.create(baker_id: 1, name: "Rose Cake", description: "Rose cake", image: "https://lh3.googleusercontent.com/pw/ACtC-3eKC4wfZPu1SsDjYu3vabYJUAtVnL94wv7hqEFASnvVCq0DCLogPxStLtEiVoH1bSXsBcv1yTxR6M2pcie8bg1HAcAbOyMXkeVxo8e1Pl7BpKd7OAq8tt3wcl5TDSBXO_yh2ZIEd30YQDTnh5QvOygZ=w1080-h1202-no?authuser=0")
+Pastry.create(baker_id: 1, name: "Beer Cake", description: "Beer cake", image: "https://s3-media0.fl.yelpcdn.com/bphoto/L0yEaIw4nrlutAmOjepa4w/o.jpg")
+Pastry.create(baker_id: 3, name: "Birthday Cake", description: "Disney Theme Cake", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPbQAJGh-oeW_inDwYn51vpLaCgR0VK5YWng&usqp=CAU")
+Pastry.create(baker_id: 3, name: "Croissant", description: "A buttery, flaky, viennoiserie French pastry", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcMuC5bzEL_ecE-5AHZ7aSbuS5GCy_W5OHcQ&usqp=CAU")
+Pastry.create(baker_id: 4, name: "Napoleon", description: "made of flaky, buttery puff pastry layered between sweet vanilla custard.", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeiFSlmmgXOP1UFgy51SNSED6JerGbYmxUVA&usqp=CAU")
+Pastry.create(baker_id: 2, name: "Rainbow Cake", description: "Not only is this rainbow cake beautiful and colorful, but it also tastes really delicious. Made from my famous white velvet cake recipe and easy buttercream, this rainbow cake makes the perfect special occasion cake!", image: "https://storcpdkenticomedia.blob.core.windows.net/media/recipemanagementsystem/media/recipe-media-files/recipes/retail/x17/rainbow-cake760x580.jpg?ext=.jpg")
 Pastry.create(baker_id: 2, name: "Mille-Feuille", description: "Meaning “a thousand layers” in French, the mille-feuille is known for its seemingly countless layers of puff pastry.", image: "https://www.bakefromscratch.com/wp-content/uploads/2019/02/CremeFraiche336SWS-KR4x5_2-1.jpg")
-Pastry.create(baker_id: 3, name: "Double Chocolate Cookie Bar", description: "Double Chocolate Chip Cookie Bars is the perfect mix between cookie & brownie – A crisp, chewy, chocolate lover’s dream.", image: "https://www.errenskitchen.com/wp-content/uploads/2014/05/double-chocolate-chip-bars-1.jpg")
+Pastry.create(baker_id: 4, name: "Double Chocolate Cookie Bar", description: "Double Chocolate Chip Cookie Bars is the perfect mix between cookie & brownie – A crisp, chewy, chocolate lover’s dream.", image: "https://www.errenskitchen.com/wp-content/uploads/2014/05/double-chocolate-chip-bars-1.jpg")
 
 
 # t.belongs_to :baker, null: false, foreign_key: true
